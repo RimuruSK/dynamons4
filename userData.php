db1fda70388c465cb625abcabe22d569541cd11f
RimuruSK <158226399+RimuruSK@users.noreply.github.com>
2025-03-15 11:14:10 +0700

    Update userData.php

--- userData.php
+++ userData.php
@@ -10,9 +10,9 @@
         "accountType": 0,
         "level": 1,
         "premiumPoints": 1000000,
-        "experience": 989,
+        "experience": 2877,
         "experienceNextLevel": 33100,
-        "experiencePercents": 2.9879,
+        "experiencePercents": 8.6918,
         "bonusExp": 50,
         "premium": 0,
         "startPoints": 3,
@@ -34,17 +34,17 @@
             "level": "1",
             "premium": 0,
             "spawnTimer": 0,
-            "time": 1742007166,
+            "time": 1742011821,
             "bonusExp": 50,
             "access": 0,
-            "dailyQuests": "328731132,1,player,328731133,5,strawberry,328731134,4,ghost",
+            "dailyQuests": "328731132,1,player,328731134,4,ghost",
             "skins": "pets,1,2,3",
             "countryCode": "VN",
-            "experience": "989",
+            "experience": "2877",
             "experienceNextLevel": "33100",
-            "experiencePercents": "2.9879"
+            "experiencePercents": "8.6918"
         },
-        "authToken": "9f932491f17e6b81b941831702e0a47f"
+        "authToken": "f6ecde9664941499426dedd56e8b3f3e"
     },
     "dailyQuests": {
         "1": {
@@ -52,24 +52,19 @@
             "type": 1,
             "amount": 5,
             "what": "player",
-            "reward": 100000000000000,
+            "reward": 1000000000,
             "progress": 5
         },
         "2": {
-            "id": 328731133,
-            "type": 5,
-            "amount": 5,
-            "what": "strawberry",
-            "reward": 100000000000000,
-            "progress": 8
+            "startTime": 9
         },
         "3": {
             "id": 328731134,
             "type": 4,
             "amount": 1,
             "what": "ghost",
-            "reward": 100000000000000,
-            "progress": 1
+            "reward": 1000000000,
+            "progress": 5
         }
     },
     "popups": [],
@@ -79,7 +74,7 @@
             "ip": "wss:\/\/46-183-219-238.evoworld.io",
             "port": "1600",
             "location": "EUNE",
-            "players": 170,
+            "players": 195,
             "maxPlayers": 300
         },
         {
@@ -87,7 +82,7 @@
             "ip": "wss:\/\/162-19-84-53.evoworld.io",
             "port": "1600",
             "location": "EUW",
-            "players": 101,
+            "players": 121,
             "maxPlayers": 300
         },
         {
@@ -95,7 +90,7 @@
             "ip": "wss:\/\/144-48-104-66.evoworld.io",
             "port": "1600",
             "location": "NA",
-            "players": 277,
+            "players": 211,
             "maxPlayers": 300
         },
         {
@@ -103,7 +98,7 @@
             "ip": "wss:\/\/144-48-104-66.evoworld.io",
             "port": "2600",
             "location": "NA",
-            "players": 104,
+            "players": 96,
             "maxPlayers": 300
         },
         {
@@ -111,7 +106,7 @@
             "ip": "wss:\/\/108-181-68-193.evoworld.io",
             "port": "1600",
             "location": "SA",
-            "players": 149,
+            "players": 96,
             "maxPlayers": 300
         },
         {
@@ -119,7 +114,7 @@
             "ip": "wss:\/\/15-235-180-40.evoworld.io",
             "port": "1601",
             "location": "ASSE",
-            "players": 294,
+            "players": 300,
             "maxPlayers": 300
         },
         {
@@ -127,7 +122,7 @@
             "ip": "wss:\/\/15-235-180-40.evoworld.io",
             "port": "2601",
             "location": "ASSE",
-            "players": 116,
+            "players": 97,
             "maxPlayers": 300
         },
         {
@@ -135,7 +130,7 @@
             "ip": "wss:\/\/148-113-47-32.evoworld.io",
             "port": "1600",
             "location": "ASS",
-            "players": 95,
+            "players": 115,
             "maxPlayers": 300
         },
         {
@@ -143,7 +138,7 @@
             "ip": "wss:\/\/181-215-205-204.evoworld.io",
             "port": "1600",
             "location": "ASW",
-            "players": 96,
+            "players": 107,
             "maxPlayers": 300
         },
         {
@@ -151,7 +146,7 @@
             "ip": "wss:\/\/139-99-197-162.evoworld.io",
             "port": "1601",
             "location": "AUS",
-            "players": 94,
+            "players": 98,
             "maxPlayers": 250
         }
     ]
