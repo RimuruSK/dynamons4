6d8b6406cc14cc109e5e9b4f8d8b3f3b4052c7a7
RimuruSK <158226399+RimuruSK@users.noreply.github.com>
2025-03-15 09:00:22 +0700

    Create userData.php

---
+++ userData.php
@@ -0,0 +1,158 @@
+{
+    "success": true,
+    "user": {
+        "loadFiles": [],
+        "guest": false,
+        "id": 101547836,
+        "login": "LazyNeko",
+        "loginDisplay": "LazyNeko",
+        "name": "LazyNeko",
+        "accountType": 0,
+        "level": 45,
+        "premiumPoints": 0,
+        "experience": 9876543212025,
+        "experienceNextLevel": 9876543212026,
+        "experiencePercents": 9.4,
+        "bonusExp": 800,
+        "premium": 1,
+        "startPoints": 3,
+        "spawnTimer": 0,
+        "discordLogged": false,
+        "access": 0,
+        "countryCode": "VN",
+        "specialCode": "SkyBS",
+        "specialCodeBonus": 0,
+        "specialCodeValid": "2030-12-31 23:59:59",
+        "showPremiumOffer": true,
+        "skins": "pets,1,2,3",
+        "profileKey": "62110498ce01e54e3697709bf38ad8cf",
+        "authData": {
+            "id": "101547836",
+            "guest": false,
+            "registerDate": "14.03.2025",
+            "login": "LazyNeko",
+            "level": "45",
+            "premium": 1,
+            "spawnTimer": 0,
+            "time": 1742003789,
+            "bonusExp": 800,
+            "access": 0,
+            "dailyQuests": "328731132,1,player,328731133,5,strawberry,328731134,4,ghost",
+            "skins": "pets,1,2,3",
+            "countryCode": "VN",
+            "experience": "9876543212025",
+            "experienceNextLevel": "9876543212026",
+            "experiencePercents": "9.4"
+        },
+        "authToken": "5bb70d30d920c4e95313b3043041bec2"
+    },
+    "dailyQuests": {
+        "1": {
+            "id": 328731132,
+            "type": 1,
+            "amount": 5,
+            "what": "player",
+            "reward": 88888888,
+            "progress": 0
+        },
+        "2": {
+            "id": 328731133,
+            "type": 5,
+            "amount": 5,
+            "what": "strawberry",
+            "reward": 999999999,
+            "progress": 0
+        },
+        "3": {
+            "id": 328731134,
+            "type": 4,
+            "amount": 1,
+            "what": "ghost",
+            "reward": 10000000000,
+            "progress": 0
+        }
+    },
+    "popups": [],
+    "servers": [
+        {
+            "name": "North-East Europe 1",
+            "ip": "wss:\/\/46-183-219-238.evoworld.io",
+            "port": "1600",
+            "location": "EUNE",
+            "players": 139,
+            "maxPlayers": 300
+        },
+        {
+            "name": "West Europe 1",
+            "ip": "wss:\/\/162-19-84-53.evoworld.io",
+            "port": "1600",
+            "location": "EUW",
+            "players": 95,
+            "maxPlayers": 300
+        },
+        {
+            "name": "North America 1",
+            "ip": "wss:\/\/144-48-104-66.evoworld.io",
+            "port": "1600",
+            "location": "NA",
+            "players": 287,
+            "maxPlayers": 300
+        },
+        {
+            "name": "North America 2",
+            "ip": "wss:\/\/144-48-104-66.evoworld.io",
+            "port": "2600",
+            "location": "NA",
+            "players": 187,
+            "maxPlayers": 300
+        },
+        {
+            "name": "South America 1",
+            "ip": "wss:\/\/108-181-68-193.evoworld.io",
+            "port": "1600",
+            "location": "SA",
+            "players": 179,
+            "maxPlayers": 300
+        },
+        {
+            "name": "South-East Asia 1",
+            "ip": "wss:\/\/15-235-180-40.evoworld.io",
+            "port": "1601",
+            "location": "ASSE",
+            "players": 299,
+            "maxPlayers": 300
+        },
+        {
+            "name": "South-East Asia 2",
+            "ip": "wss:\/\/15-235-180-40.evoworld.io",
+            "port": "2601",
+            "location": "ASSE",
+            "players": 79,
+            "maxPlayers": 300
+        },
+        {
+            "name": "South Asia 1",
+            "ip": "wss:\/\/148-113-47-32.evoworld.io",
+            "port": "1600",
+            "location": "ASS",
+            "players": 69,
+            "maxPlayers": 300
+        },
+        {
+            "name": "West Asia 1",
+            "ip": "wss:\/\/181-215-205-204.evoworld.io",
+            "port": "1600",
+            "location": "ASW",
+            "players": 70,
+            "maxPlayers": 300
+        },
+        {
+            "name": "Australia 1",
+            "ip": "wss:\/\/139-99-197-162.evoworld.io",
+            "port": "1601",
+            "location": "AUS",
+            "players": 89,
+            "maxPlayers": 250
+        }
+    ]
+}
